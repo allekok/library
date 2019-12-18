4acd3774da7736ec51f2b317f9d138e7c5d945d3
allekok <one@allekok.com>
2019-12-18 12:37:44 +0330

    Use compressed stylesheets.

--- www/index.php
+++ www/index.php
@@ -20,8 +20,8 @@ $html_attr = "dir='{$html_dir}' lang='{$html_lang}'";
 	</title>
 	<meta charset="utf-8" />
 	<link rel="stylesheet" href="<?php 
-				     echo $dark ? "style-dark.css" :
-					  "style.css";
+				     echo $dark ? "style-dark-comp.css" :
+					  "style-comp.css";
 				     ?>" />
 	<meta name='viewport' content='width=device-width, initial-scale=1' />
 	<style>
