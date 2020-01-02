865163cb0587d71cd7242dc7bdce71d2ec12374c
Payam Bapiri <payambapiri.97@gmail.com>
2020-01-02 12:27:01 +0330

    change default fields

--- www/constants.php
+++ www/constants.php
@@ -5,8 +5,8 @@ const AVAILABLE_LANGS = ["ckb", "en" , "fa"];
 const LANGS_LIT = ["کوردی","English","فارسی"];
 const SITE_LANG_DEF = "fa";
 
-const DEF_COLS = ["fa" => ["ردیف","عنوان‏","پديدآور"],
-		  "en" => ["id","Title‎","Author‎"]];
+const DEF_COLS = ["fa" => ["شماره راهنما (کنگره)‏","عنوان‏","پديدآور"],
+		  "en" => ["LC No.‎","Title‎","Author‎"]];
 
 const TABLES_LIT = ["english books", "persian books"];
 ?>
