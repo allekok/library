1e30d35d472acaad2a5d3f5f2b168d42b8707170
Payam <payambapiri.97@gmail.com>
2022-04-17 19:02:27 +0430

    xml/*: Clean up codes

--- xml/read-from-xml.php
+++ xml/read-from-xml.php
@@ -1,32 +1,28 @@
 <?php
-require('xml-lib.php');
+require_once("xml-lib.php");
 
 $LANG = (@$argv[1] == "fa") ? "fa" : "en";
-mkdir("out/{$LANG}", 0755, TRUE);
-
 $PATH = "data/SIMORGH-{$LANG}.XML";
-$XML = file_get_contents($PATH);
 
+mkdir("out/{$LANG}", 0755, TRUE);
+$XML = file_get_contents($PATH);
 $TOKENS = tokenize($XML);
 $RECORDS = extract_tags($TOKENS, "<Record", "</Record>");
 
-foreach($RECORDS as $I => $R)
-{
+foreach($RECORDS as $I => $R) {
 	$FIELDS_NAMES = tag_names($R, "<Field");
 	$FIELDS = extract_tags($R, "<Field", "</Field>");
 
 	$O = fopen("out/{$LANG}/{$I}", "w");
 
-	foreach($FIELDS as $II => $F)
-	{
+	foreach($FIELDS as $II => $F) {
 		$SUBFIELDS_NAMES = tag_names($F, "<SubField");
 		$SUBFIELDS = extract_tags($F, "<SubField", "</SubField>");
 
 		$F_NAME = sanitize_string($FIELDS_NAMES[$II]);
 		fwrite($O, "{$F_NAME}:\n");
 
-		foreach($SUBFIELDS as $III => $S)
-		{
+		foreach($SUBFIELDS as $III => $S) {
 			$S_NAME = sanitize_string($SUBFIELDS_NAMES[$III]);
 			$S_0 = sanitize_string($S[0]);
 			fwrite($O, "\t{$S_NAME}: {$S_0}\n");
--- xml/xml-lib.php
+++ xml/xml-lib.php
@@ -1,34 +1,30 @@
 <?php
 function tokenize($S) {
 	$TOKENS = [];
-	
-	for($I = 0; $I < strlen($S); )
-	{
+
+	for($I = 0; $I < strlen($S); ) {
 		$C = substr($S, $I, 1);
-		if($C == "\n" or $C == "\r")
-		{
+
+		if($C == "\n" or $C == "\r") {
 			$I++;
 			continue;
 		}
-		
-		if($C == '<')
-		{
-			$T = '';
-			while(FALSE !== ($C = substr($S, $I, 1)))
-			{
+
+		if($C == "<") {
+			$T = "";
+			while($I < strlen($S)) {
+				$C = substr($S, $I++, 1);
 				$T .= $C;
-				$I++;
-				if($C == '>')
-					break 1;
+				if($C == ">")
+					break;
 			}
 			$TOKENS[] = $T;
 		}
-		else
-		{
-			$T = '';
-			while(FALSE !== ($C = substr($S, $I, 1)))
-			{
-				if($C == '<')
+		else {
+			$T = "";
+			while($I < strlen($S)) {
+				$C = substr($S, $I, 1);
+				if($C == "<")
 					break 1;
 				$T .= $C;
 				$I++;
@@ -36,97 +32,91 @@ function tokenize($S) {
 			$TOKENS[] = $T;
 		}
 	}
+
 	return $TOKENS;
 }
 
-function extract_tags (array $A, $start, $end, $N=-1)
-{
+function extract_tags($A, $start, $end, $N=-1) {
 	$I = 0;
 	$R = [];
 	$CARRY = FALSE;
-	foreach($A as $O)
-	{
+
+	foreach($A as $O) {
 		if($N == $I)
 			break;
-		
-		if($O == $end)
-		{
+
+		if($O == $end) {
 			$CARRY = FALSE;
 			$I++;
 		}
-		elseif(strpos($O, $start) === 0)
-		{
+		elseif(strpos($O, $start) === 0) {
 			$CARRY = TRUE;
 			continue;
 		}
 
 		if($CARRY)
-		{
 			$R[$I][] = $O;
-		}
 	}
+
 	return $R;
 }
 
-function tag_names ($ARR, $TAG)
-{
-	$TAG_LEN = strlen($TAG);
+function tag_names($ARR, $TAG) {
 	$RES = [];
-	foreach($ARR as $T)
-	{
+	$TAG_LEN = strlen($TAG);
+
+	foreach($ARR as $T) {
 		if(substr($T, 0, $TAG_LEN) != $TAG)
 			continue;
 
 		preg_match_all("/ Name=\".+\" /", $T, $R);
-		if($R[0]) $RES[] = substr($R[0][0], 7, -2);
+		if($R[0])
+			$RES[] = substr($R[0][0], 7, -2);
 	}
+
 	return $RES;
 }
 
-function sanitize_string ($S)
-{
+function sanitize_string($S) {
 	$S = trim($S);
-
-	if(mb_substr($S,-2) == " ‏")
+	if(mb_substr($S, -2) == " ‏")
 		$S = mb_substr($S, 0, -2);
-
 	return $S;
 }
 
-function list_fields ($LANG="en")
-{
+function list_fields($LANG="en") {
 	$RES = [];
 	$PATH = "data/SIMORGH-{$LANG}.XML";
 	$S = file_get_contents($PATH);
 	$TOKENS = tokenize($S);
 	$RECORDS = extract_tags($TOKENS, "<Record", "</Record>");
-	foreach($RECORDS as $I => $R)
-	{
+
+	foreach($RECORDS as $I => $R) {
 		$FIELDS = extract_tags($R, "<Field", "</Field>");
 		$FIELDS_NAMES = tag_names($R, "<Field");
 
-		foreach($FIELDS as $II => $F)
-		{
-			$SUBFIELDS = extract_tags($F, "<SubField", "</SubField>");
+		foreach($FIELDS as $II => $F) {
+			$SUBFIELDS = extract_tags($F,
+						  "<SubField",
+						  "</SubField>");
 			$SUBFIELDS_NAMES = tag_names($F, "<SubField");
 
 			$F_NAME = sanitize_string($FIELDS_NAMES[$II]);
-			if(! in_array($F_NAME, $RES))
+			if(!in_array($F_NAME, $RES))
 				$RES[] = $F_NAME;
 		}
 	}
+
 	return $RES;
 }
 
-function sql_columns ($FIELDS, $LANG='en')
-{
-	if($LANG == 'en')
+function sql_columns($FIELDS, $LANG="en") {
+	if($LANG == "en")
 		$S = "`id`";
-	elseif($LANG == 'fa')
+	elseif($LANG == "fa")
 		$S = "`ردیف`";
 	$S .= " INT(16) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
-	foreach($FIELDS as $F)
-	{
+	foreach($FIELDS as $F) {
 		$S .= ",`$F` TEXT";
 	}
 	return $S;
